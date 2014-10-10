4464459f276bab7f769eab8f0bae60b2b4d31b83
koichi-ezato <koichi-ezato@osc-corp.co.jp>
2014-10-10 18:31:10 +0900

    区分：新規作成 使用ライブラリ：mojimoji ライブラリ概要：全角半角変換ライブラリ 処理概要 全角半角変換処理のサンプルを新規作成

---
+++ PythonSample/src/pythonsample.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="OSC-ezato"
+__date__ ="$2014/10/10 18:25:30$"
+
+if __name__ == "__main__":
+    print "Hello World"
---
+++ PythonSample/src/zenhanexchange.py
@@ -0,0 +1,52 @@
+#! /usr/bin/python
+# -*- encoding: utf-8 -*-
+import mojimoji
+
+# unicodeをutf-8にエンコードする
+def unicode_to_utf8(r):
+	return r.encode('utf-8')
+
+# 全角文字を全て半角文字に変換
+print '----- 全角→半角変換 -----\r\n'
+print 'target:アイウａｂｃ０１２\r\n'
+
+zenAll = u'アイウａｂｃ０１２'
+r = mojimoji.zen_to_han(zenAll)
+
+print unicode_to_utf8(r)
+
+# 全角カナ以外の全角文字を全て半角に変換
+r = mojimoji.zen_to_han(zenAll, kana = False)
+print unicode_to_utf8(r)
+
+# 全角数字以外の全角文字を全て半角に変換
+r = mojimoji.zen_to_han(zenAll, digit = False)
+print unicode_to_utf8(r)
+
+# 全角アスキー文字以外の全角文字を全て半角に変換
+r = mojimoji.zen_to_han(zenAll, ascii = False)
+print unicode_to_utf8(r)
+print '\r\n----- 全角→半角変換 -----\r\n'
+
+# 半角文字を全て全角文字に変換
+print '----- 半角→全角変換 -----\r\n'
+print 'target:ｱｲｳabc012\r\n'
+
+hanAll = u'ｱｲｳabc012'
+
+# 半角文字を全て全角文字に変換
+r = mojimoji.han_to_zen(hanAll)
+print unicode_to_utf8(r)
+
+# 半角カナ以外の半角文字を全て全角に変換
+r = mojimoji.han_to_zen(hanAll, kana = False)
+print unicode_to_utf8(r)
+
+# 半角数字以外の半角文字を全て全角に変換
+r = mojimoji.han_to_zen(hanAll, digit = False)
+print unicode_to_utf8(r)
+
+# 半角アスキー文字以外の半角文字を全て全角に変換
+r = mojimoji.han_to_zen(hanAll, ascii = False)
+print unicode_to_utf8(r)
+print '\r\n----- 半角→全角変換 -----\r\n'
